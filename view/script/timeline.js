531f23c8b6fd189571c5351f3f1fdc8275ffa545
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-10-26 21:04:24 +0700

    update

--- view/script/timeline.js
+++ view/script/timeline.js
@@ -1,8 +1,5 @@
 import { API } from './api.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Đoạn mã JavaScript của bạn ở đây
-
+async function callImg () {
     $.ajax({
         type: 'GET',
         url: API + '/social-media/controller/usercontroller/read.php',
@@ -12,23 +9,35 @@ document.addEventListener('DOMContentLoaded', function() {
         success: function (response) {
             var numFriend = 0;
             if (response.data == null) {
+
             } else {
                 numFriend = response.data.length;
-                let $str = response.data.map(function (user) {
-                    return `<li>
-                                <img src="${user.avatar_url}" width="116" alt="">
-                                <div class="sugtd-frnd-meta">
-                                    <a href="#" title="">${user.full_name}</a>
-                                    <span>1 mutual friend</span>
-                                    <ul class="add-remove-frnd">
-                                        <li class="add-tofrndlist"><a href="#" title="Add friend"><i class="fa fa-user-plus"></i></a></li>
-                                        <li class="remove-frnd"><a href="#" title="remove friend"><i class="fa fa-user-times"></i></a></li>
-                                    </ul>
-                                </div>
-                            </li>`;
-                });
-                $('#recomment-friends').html($str);
+                let str = response.data.map(function (user) {
+                    return `
+                        <li>
+                            <img src="${user.avatar_url}" alt="">
+                            <div class="sugtd-frnd-meta">
+                                <a href="#" title="">${user.full_name}</a>
+                                <span>1 mutual friend</span>
+                                <ul class="add-remove-frnd">
+                                    <li class="add-tofrndlist"><a href="#" title="Add friend"><i
+                                                class="fa fa-user-plus"></i></a></li>
+                                    <li class="remove-frnd"><a href="#" title="remove friend"><i
+                                                class="fa fa-user-times"></i></a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    `
+                }).join('');
+
+                const targetDiv = document.querySelector('#recomment-friends');
+                console.log('targetDiv: ', targetDiv);
+                targetDiv.innerHTML += str;
+
             }
         }
     });
-});
+}
+
+await document.addEventListener('DOMContentLoaded', callImg);
+
